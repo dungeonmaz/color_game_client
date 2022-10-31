5238ea3e87d1a36f8c17492f78ad7cd2d73518f0
Mazdog <dungeonmaz@gmail.com>
2022-10-31 19:51:01 +0700

    Commit Test Test commit description

--- src/components/home/Changelog.jsx
+++ src/components/home/Changelog.jsx
@@ -1,18 +1,19 @@
 import { Stack } from '@mui/system'
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import ChangelogCard from './ChangelogCard'
 
 const Changelog = () => {
-  
+  const [commits, setCommits] = useState()
+
   useEffect(() => {
     fetch("https://api.github.com/repos/dungeonmaz/color_game_client/commits")
-    .then(res => res.json())
-    .then(res => console.log(res))
+      .then(res => res.json())
+      .then(res => setCommits(res))
   }, [])
 
   return (
     <Stack p="1rem">
-      <ChangelogCard name={"0.1"} description={'Create Project'}/>
+      <ChangelogCard name={"0.1"} description={commits[0].commit.message} />
     </Stack>
   )
 }
